ef5dbaaeff93f5a70d566082be4935c792cc1061
Arthur SAUVEZIE <65153093+arthursvz@users.noreply.github.com>
2025-07-21 14:07:36 +0000

    delete

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  base: '/arthursvz.github.io/', // Remplace par le nom de ton repo
-  plugins: [svelte()]
-});
